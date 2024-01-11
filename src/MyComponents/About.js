6d514c955e375bc41acb80e644338fa2c09298fd
shivendra-999 <salukheshiv123@gmail.com>
2024-01-11 15:38:36 +0530

    add

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      About Component
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quaerat velit provident? Reprehenderit alias doloremque consequatur deserunt excepturi nobis blanditiis voluptatem suscipit dicta? Commodi quisquam similique repudiandae? Exercitationem, vel minus.</p>
+    </div>
+  )
+}
+
+export default About
